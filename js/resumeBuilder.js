65e0abf72bf3b9fa57ead48bbd1f1a891ca89014
U-STATES\aowens <aowens@sts0738.states.win.statesind.com>
2016-07-08 16:29:44 -0700

    Updated resume and index

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var formattedName = HTMLheaderName.replace("%data%", "Andrew Owens");
+var formattedTitle = HTMLheaderRole.replace("%data%", "Front End Developer");
+
+$('#header').append(formattedName);
+$('#header').append(formattedTitle);
